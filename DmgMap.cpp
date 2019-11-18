3eeb7184d19cf959ee2932edfa89f6fe1e6d992e
grzesti868 <gregustich@gmail.com>
2019-11-18 21:26:24 +0100

    modified:   AMF_v1.pro.user 	modified:   DmgMap.cpp 	modified:   DmgMap.h

--- DmgMap.cpp
+++ DmgMap.cpp
@@ -4,15 +4,85 @@ DmgMap::DmgMap(QImage &i_raw, QImage &i_dmg)
     :m_raw{i_raw},m_dmg{i_dmg}
 {}
 DmgMap::DmgMap(const DmgMap& i_src)
-    :m_raw{i_src.m_raw},m_dmg{i_src.m_dmg},m_map{i_src.m_map}
-{}
-DmgMap::DmgMap(DmgMap&& i_src)
-    :m_raw{i_src.m_raw},m_dmg{i_src.m_dmg},m_map{i_src.m_map}
+    :m_raw{i_src.m_raw},m_dmg{i_src.m_dmg},m_map{i_src.m_map},m_2Dwidth{i_src.m_2Dwidth},m_2Dheight{i_src.m_2Dheight}
 {
+    try
+    {
+        if(i_src.m_2Dmap != nullptr)
+        {
+            m_2Dmap= new bool*[m_2Dheight];
+            m_2Dmap[0] = new bool[m_2Dheight*m_2Dwidth];
+                for (size_t i = 1; i < m_2Dheight; i++)
+                    m_2Dmap[i] = &m_2Dmap[0][i*m_2Dwidth];
+
+        }
+    }
+    catch(std::exception& err)
+    {
+        qInfo()<<"Error copy contructor: "<<err.what();
+    }
+}
+DmgMap::DmgMap(DmgMap&& i_src)
+    :m_raw{i_src.m_raw},m_dmg{i_src.m_dmg},m_map{i_src.m_map},m_2Dwidth{i_src.m_2Dwidth},m_2Dheight{i_src.m_2Dheight}
+{
+    try
+    {
+        if(i_src.m_2Dmap!=nullptr)
+         {
+             m_2Dmap[0][0]=&i_src.m_2Dmap[0][0];
+             i_src.m_2Dmap=nullptr;
+         }
     i_src.~DmgMap();
+    }
+    catch (std::exception& err)
+    {
+       qInfo()<<"Error move contructor: "<<err.what();
+    }
 }
 
-void DmgMap::makeMap()
+void DmgMap::release2Dtab()
+{
+    try
+    {
+       if(m_2Dmap!=nullptr)
+        {
+           delete[] m_2Dmap[0];
+           m_2Dmap=nullptr;
+           m_2Dwidth=0;
+           m_2Dheight=0;
+        }
+    }
+    catch(std::exception &err)
+    {
+        std::cout<<err.what();
+    }
+}
+
+DmgMap::~DmgMap()
+{
+    /*
+    if(map_R != nullptr)
+    {
+        delete[] map_R;
+        map_R = nullptr;
+    }
+
+    if(map_G != nullptr)
+    {
+        delete[] map_G;
+        map_G = nullptr;
+    }
+
+    if(map_B != nullptr)
+    {
+        delete[] map_B;
+        map_B = nullptr;
+    }
+    */
+    release2Dtab();
+}
+
+void DmgMap::makeMapImages()
 {
     if(!m_raw.isNull() && !m_dmg.isNull()  && m_raw.width()==m_dmg.width() && m_raw.height()==m_dmg.height())
         {
@@ -35,3 +105,47 @@ void DmgMap::makeMap()
 
         }
 }
+bool DmgMap::isPixelSame(short *raw_R, short *raw_G, short *raw_B, short *dmg_R, short *dmg_G, short *dmg_B,int coord)
+{
+    try
+    {
+     if(raw_R[coord]==dmg_R[coord] && raw_G[coord]==dmg_G[coord] && raw_B[coord]==dmg_B[coord])
+            return true;
+        else
+           return false;
+    }
+    catch(std::exception& err)
+    {
+        std::cout<<err.what();
+        return false;
+    }
+}
+void DmgMap::make2DMap(short ** i_rawR,short ** i_rawG,short ** i_rawB, short ** i_dmgR,short ** i_dmgG,short ** i_dmgB, size_t i_width,size_t i_height)
+{
+    try
+    {
+        release2Dtab();
+        if(m_2Dmap == nullptr)
+        {
+
+                m_2Dmap= new bool*[i_height];
+                m_2Dmap[0] = new bool[i_height*i_width];
+                for (size_t i = 1; i < i_height; i++)
+                    m_2Dmap[i] = &m_2Dmap[0][i*i_width];
+        }
+
+        for(size_t i=0;i<i_width;i++)
+            for(size_t j=0; j<i_height;j++)
+            {
+                 if(i_rawR[i][j]==i_dmgR[i][j] && i_rawG[i][j]==i_dmgG[i][j] && i_rawB[i][j]==i_dmgB[i][j])
+                    m_2Dmap[i][j]=true;
+                 else
+                     m_2Dmap[i][j]=false;
+            }
+    }
+    catch(std::exception& err)
+    {
+        qInfo()<<"Erorr make2Dmap method: "<<err.what();
+    }
+}
+
--- DmgMap.h
+++ DmgMap.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QImage>
-
+#include <iostream>
+#include <QtDebug>
 class DmgMap
 {
 protected:
@@ -8,14 +9,28 @@ protected:
     QImage m_dmg;
     QImage m_map;
 
-    void makeMap() ;
+   // std::shared_ptr<short>;
+   // short * map_R = nullptr;
+   // short * map_G = nullptr;
+   // short * map_B = nullptr;
+    bool ** m_2Dmap = nullptr;
+    unsigned int m_2Dwidth;
+    unsigned int m_2Dheight;
+
+    void release2Dtab() ;
+
+
 
 public:
     DmgMap()=default;
     DmgMap(QImage &i_raw,QImage &i_dmg);
-    ~DmgMap()=default;
+    ~DmgMap();
     DmgMap(const DmgMap&);
     DmgMap(DmgMap&&);
+
+    void makeMapImages() ;
+    void make2DMap(short ** i_rawR,short ** i_rawG,short ** i_rawB, short ** i_dmgR,short ** i_dmgG,short ** i_dmgB, size_t i_width,size_t i_height);
+    bool isPixelSame(short *raw_R,short *raw_G,short *raw_B,short *dmg_R,short *dmg_G,short *dmg_B,int coord);
 
     void setRaw(QImage &i_raw) { if(!i_raw.isNull()) m_raw=i_raw; }
     void setDmg(QImage &i_dmg) { if(!i_dmg.isNull()) m_dmg=i_dmg; }
@@ -23,6 +38,10 @@ public:
 
     QImage getRaw() const { return m_raw; }
     QImage getDmg() const { return m_dmg; }
-    QImage getMap() { if(m_map.isNull()) makeMap(); return m_map; }
+    QImage getMap() { if(m_map.isNull()) makeMapImages(); return m_map; }
+    //short * getMapR(){return map_R;}
+    //short * getMapG(){return map_G;}
+    //short * getMapB(){return map_B;}
+    bool ** get2Dmap(){ return m_2Dmap; }
 };
 
