601908b024df4a7fc980c7722fd974b326b7ebc1
BlazejNycz <57407984+BlazejNycz@users.noreply.github.com>
2019-11-19 17:21:45 +0100

    Add files via upload

---
+++ baseFilter.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<vector>
+using namespace std;
+
+class baseFilter
+{
+private:
+	vector<double> parameters;
+	char** fixedPixel = nullptr;
+public:
+	baseFilter();
+	~baseFilter();
+	void setParameters(vector<double> iParameters) { parameters = iParameters; };
+	vector<double> getParameters() { return parameters; }
+	void setFixedPixel(int xPixel, int yPixel);
+	char** getFixedPixel() {return fixedPixel;};
+
+	void takePicture(char tabR, char tabG, char tabB);
+	// jakiTyp? returnFixedPicture();
+	virtual void Correction()=0;	
+};
+
