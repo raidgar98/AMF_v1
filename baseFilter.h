edf4204d37e7e3300a06b96193d97dc3b816d51c
BlazejNycz <57407984+BlazejNycz@users.noreply.github.com>
2019-11-26 16:43:04 +0100

    Add files via upload

--- baseFilter.h
+++ baseFilter.h
@@ -5,22 +5,23 @@ using namespace std;
 class baseFilter
 {
 private:
-	vector<double> parameters; //Parametry potrzebne konkretnemu filtrowi
-	short** fixedPixelMap = nullptr; //mapa naprawionych pixeli
-	//short*** pictureToFix = nullptr; //wskaŸnik do naprawionego obrazu
+	//parametry potrzebne konkretnemu filtrowi trzeba dopisaæ w klasach dziedzicz¹cych
+	uint32_t ** fixedPixelMap = nullptr; //mapa naprawionych pixeli
+	short*** picture = nullptr; //wskaŸnik do obrazu
+	//wymiary obrazka
+	uint32_t pictureHeight=0;
+	uint32_t pictureLeght=0;
 
 public:
 	baseFilter();
-	~baseFilter() { if (fixedPixelMap != nullptr) delete[] fixedPixelMap; };
-	void setParameters(vector<double> iParameters) { parameters = iParameters; };
-	vector<double> getParameters() { return parameters; }
-	void setFixedPixel(int xPixel, int yPixel) {
-		if (fixedPixelMap != nullptr) { fixedPixelMap[xPixel][yPixel] = 1; }
-	}; //zaznacza na mapie naprawione pixele
-	short** getFixedPixel() {return fixedPixelMap;}; //zwraca wskaŸnik do mapy z naprawionymi pixelami
+	~baseFilter();
+	virtual void setParameters(vector<double> iParameters) =0;//ustawianie parametrów jest za pomoc¹ wektora, w klasach pochodnych trzeba zdefiniowaæ, która z jego sk³adowych bêdzie wype³nia³a dany parametr
+	//w klasach pochodnych trzeba dodaæ gettery do dodanych parametrów
+	void setFixedPixel(int xPixel, int yPixel) ; //zaznacza na mapie naprawione pixele
+	uint32_t** getFixedPixel() {return fixedPixelMap;}; //zwraca wskaŸnik do mapy z naprawionymi pixelami
 
-	//void takePicture(short ***tabRGB); //pobiera obrazek do naprawy
-	//short *** returnFixedPicture(); //zwraca przyjêty obraz
+	void takePicture(short ***tabRGB, uint32_t iHeight, uint32_t iLenght);  //pobiera obrazek do naprawy
+	short *** returnFixedPicture() { return picture; }; //zwraca przyjêty obraz
 	virtual void Correction()=0; //miejsce na algorytm naprawiaj¹cy obraz
 };
 
