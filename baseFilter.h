d2f312e4be5be990ca04d044d854df3344a129e7
BlazejNycz <57407984+BlazejNycz@users.noreply.github.com>
2019-11-26 14:47:08 +0100

    Add files via upload

--- baseFilter.h
+++ baseFilter.h
@@ -5,18 +5,22 @@ using namespace std;
 class baseFilter
 {
 private:
-	vector<double> parameters;
-	char** fixedPixel = nullptr;
+	vector<double> parameters; //Parametry potrzebne konkretnemu filtrowi
+	short** fixedPixelMap = nullptr; //mapa naprawionych pixeli
+	//short*** pictureToFix = nullptr; //wskaünik do naprawionego obrazu
+
 public:
 	baseFilter();
-	~baseFilter();
+	~baseFilter() { if (fixedPixelMap != nullptr) delete[] fixedPixelMap; };
 	void setParameters(vector<double> iParameters) { parameters = iParameters; };
 	vector<double> getParameters() { return parameters; }
-	void setFixedPixel(int xPixel, int yPixel);
-	char** getFixedPixel() {return fixedPixel;};
+	void setFixedPixel(int xPixel, int yPixel) {
+		if (fixedPixelMap != nullptr) { fixedPixelMap[xPixel][yPixel] = 1; }
+	}; //zaznacza na mapie naprawione pixele
+	short** getFixedPixel() {return fixedPixelMap;}; //zwraca wskaünik do mapy z naprawionymi pixelami
 
-	void takePicture(char tabR, char tabG, char tabB);
-	// jakiTyp? returnFixedPicture();
-	virtual void Correction()=0;	
+	//void takePicture(short ***tabRGB); //pobiera obrazek do naprawy
+	//short *** returnFixedPicture(); //zwraca przyjÍty obraz
+	virtual void Correction()=0; //miejsce na algorytm naprawiajπcy obraz
 };
 
