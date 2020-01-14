93722cb933ef65104214cc62de6cab72fe38d8b1
BlazejNycz <57407984+BlazejNycz@users.noreply.github.com>
2020-01-14 13:31:41 +0100

    Add files via upload

--- DamageGenerator.cpp
+++ DamageGenerator.cpp
@@ -1,57 +1,208 @@
 #include "DamageGenerator.h"
+#define M_PI       3.14159265358979323846
 
-#include <functional>
-#include <random>
+damageKind DamageGenerator::kindOfDamage = damageKind::square;
+int*** DamageGenerator::tabRGB = nullptr;
+size_t DamageGenerator::sizeX = 0;
+size_t DamageGenerator::sizeY = 0;
+DmgMap* DamageGenerator::dmgMap = nullptr;
+size_t DamageGenerator::pixelDamageNumber = 0;
 
-void DamageGenerator::makeDamage(QImage &dst, container& missing_ones, const size_t number_of_damages, const size_t damage_size) noexcept
+void DamageGenerator::rgbDamage(int xAktual, int yAktual)
 {
-    std::random_device engine;
-
-    std::uniform_int_distribution<coord_num> width  (0, static_cast<coord_num>(dst.width()  ));
-    std::uniform_int_distribution<coord_num> height (0, static_cast<coord_num>(dst.height() ));
-
-    std::function<void(const int, const std::function<coord()> , const std::function<bool(const coord &)> &)> damager;
-    std::function<bool(const coord&)> make_damage = [&](const coord& pos)
-    {
-        if(pos.x() >= static_cast<coord_num>(dst.width()) || pos.y() >= static_cast<coord_num>(dst.height())) return false;
-        dst.setPixel(pos, Qt::transparent);
-        missing_ones[pos] = false;
-        return true;
-    };
-    std::function<coord()> generate_random_coordination = [&]()
-    {
-        return coord{ width(engine), height(engine) };
-    };
-
-    switch(current_damage_kind)
-    {
-        case euclidean: damager = &DamageGenerator::euclidean_generator; break;
-        case chaos: damager = &DamageGenerator::chaos_generator; break;
-        default: damager = &DamageGenerator::euclidean_generator; break;
-    }
-
-    for(size_t i = 0; i < number_of_damages; i++)
-        damager(damage_size, generate_random_coordination, make_damage);
-
+	if (-1 < xAktual && xAktual < sizeX && -1 < yAktual && yAktual < sizeX)
+	{
+		tabRGB[xAktual][yAktual][0] = 0;
+		tabRGB[xAktual][yAktual][1] = 0;
+		tabRGB[xAktual][yAktual][2] = 0;
+		dmgMap->setDamagedPixel(xAktual, yAktual);
+	}
 }
 
-void DamageGenerator::renderMapFromRawData(const container &input, QImage& output) noexcept
+void DamageGenerator::rgbChanger(int xAktual, int yAktual,
+								int xChange = 0,int yChange = 0)
 {
-    output.fill(qRgba(0,0,0,255));
-    for(const auto& var : input)
-        output.setPixel(var.first, qRgba(255,255,255,255));
+	switch (kindOfDamage)
+	{
+	default:
+	case damageKind::square:
+	case damageKind::diamond:
+	{
+		rgbDamage( xAktual, yAktual);
+	}
+	break;
+	case damageKind::circle:
+	case damageKind::fog:
+	case damageKind::cloud:
+	{
+		rgbDamage( xAktual + xChange, yAktual + yChange);
+		rgbDamage( xAktual - xChange, yAktual + yChange);
+		rgbDamage( xAktual - xChange, yAktual - yChange);
+		rgbDamage( xAktual + xChange, yAktual - yChange);
+		rgbDamage( xAktual + yChange, yAktual + xChange);
+		rgbDamage( xAktual - yChange, yAktual + xChange);
+		rgbDamage( xAktual - yChange, yAktual - xChange);
+		rgbDamage( xAktual + yChange, yAktual - xChange);
+	}
+	break;
+	case damageKind::snow:
+	{
+		rgbDamage( xAktual + xChange, yAktual + xChange);
+		rgbDamage( xAktual - xChange, yAktual + xChange);
+		rgbDamage( xAktual - xChange, yAktual - xChange);
+		rgbDamage( xAktual + xChange, yAktual - xChange);
+		rgbDamage( xAktual, yAktual + xChange);
+		rgbDamage( xAktual, yAktual - xChange);
+		rgbDamage( xAktual - xChange, yAktual);
+		rgbDamage( xAktual + xChange, yAktual);
+	}
+	break;
+	}
 }
 
-void DamageGenerator::euclidean_generator(const int dmg_size, const std::function<coord()>& gen, const std::function<bool(const coord &)> &make_damage) noexcept
+void DamageGenerator::drawCircle(int xCenter, int yCenter)
 {
-    const coord pos = gen();
-    for(int i = -(dmg_size/2); i < (dmg_size/2); i++)
-        for(int j = -(dmg_size/2); j < (dmg_size/2); j++)
-            make_damage({pos.x() + i, pos.y() + j});
+	int radius = pow(pixelDamageNumber / M_PI, 0.5)+1;
+	if (kindOfDamage==damageKind::cloud)
+	{
+		radius /= 1.5;
+	}
+	else if (kindOfDamage == damageKind::fog)
+	{
+		radius /= 1.2;
+	}
+	double norma = 2;
+
+	for (size_t i = 0; i < radius; i++)
+	{
+		for (size_t j = i; j < radius; j+=1+(kindOfDamage == damageKind::fog))
+		{
+			std::cout << j << "\n";
+			if (pow(i, norma) + pow(j, norma)<pow(radius, norma))
+			{
+				rgbChanger(xCenter, yCenter, i, j);
+			}
+		}
+	}
 }
 
-void DamageGenerator::chaos_generator(const int dmg_size, const std::function<coord ()> &gen, const std::function<bool (const coord &)> &make_damage) noexcept
+
+DamageGenerator::DamageGenerator()
 {
-    for(int i = 0; i < dmg_size; i++)
-        make_damage(gen());
+}
+
+
+DamageGenerator::~DamageGenerator()
+{
+}
+
+
+
+void DamageGenerator::makeDamage(int***iTabRGB, size_t iSizeX, size_t iSizeY, 
+	size_t iDamageStartNumber, size_t iPixelDamageNumber, DmgMap &iDmgMap)
+{
+	tabRGB = iTabRGB;
+	sizeX = iSizeX;
+	sizeY = iSizeY;
+	dmgMap = &iDmgMap;
+	pixelDamageNumber = iPixelDamageNumber;
+
+	if (iTabRGB != nullptr && iSizeX>0 && iSizeY>0)
+	{
+		std::random_device randomGenerator;
+		int xCenter;
+		int yCenter;
+		int xAktual;
+		int yAktual;
+		std::uniform_int_distribution<int> uIntDistX(0, iSizeX);
+		std::uniform_int_distribution<int> uIntDistY(0, iSizeY);
+
+		for (size_t i = 0; i < iDamageStartNumber; i++)
+		{
+			xCenter = uIntDistX(randomGenerator);
+			yCenter = uIntDistY(randomGenerator);
+			switch (kindOfDamage)
+			{
+			default:
+			case damageKind::square:
+			{
+				for (size_t pixelNumber = 0; pixelNumber < pixelDamageNumber; pixelNumber++)
+				{
+					xAktual = xCenter - (int)sqrt(pixelDamageNumber) /
+						2 + pixelNumber % (int)sqrt(pixelDamageNumber);
+					yAktual = yCenter - (int)sqrt(pixelDamageNumber) /
+						2 + pixelNumber / (int)sqrt(pixelDamageNumber);
+					rgbChanger(xAktual, yAktual);
+				}
+				break;
+			}
+			case damageKind::diamond:
+			{
+				int cityRange = 0;
+				size_t a = 5;
+				size_t b = 4;
+				while (a <= pixelDamageNumber)
+				{
+					cityRange++;
+					b += 4;
+					a += b;
+				}
+				size_t pixelNumber = 0;
+				for (int i = -cityRange; i <= cityRange; i++)
+				{
+					for (int j = -cityRange; j <= cityRange; j++)
+					{
+						if (abs(i) + abs(j) <= cityRange)
+						{
+							xAktual = xCenter + i;
+							yAktual = yCenter + j;
+							rgbChanger(xAktual, yAktual);
+							if (++pixelNumber > pixelDamageNumber)
+							{
+								break;
+							}
+						}
+					}
+					if (pixelNumber > pixelDamageNumber)
+					{
+						break;
+					}
+				}
+				break;
+			}
+			case damageKind::circle:
+			{
+				drawCircle(xCenter, yCenter);
+			}
+			break;
+			case damageKind::fog:
+			case damageKind::cloud:
+			{
+				int numberOfCircleInCloud = 5;
+				int xTemp;
+				int yTemp;
+				int cloudRange = pow(pixelDamageNumber / M_PI, 0.5) + 1;;
+				std::uniform_int_distribution<int> 
+					uIntDistCloud(-cloudRange / 2, cloudRange / 2);
+				for (size_t i = 0; i < numberOfCircleInCloud; i++)
+				{
+					xTemp = uIntDistCloud(randomGenerator);
+					yTemp = uIntDistCloud(randomGenerator);
+					drawCircle(xCenter + xTemp, yCenter + 2 * yTemp);
+				}
+			}
+			break;
+			case damageKind::snow:
+			{
+				int radius = pixelDamageNumber/8 + 1;
+				double norma = 2;
+				for (size_t i = 0; i<radius; i++)
+				{
+					rgbChanger(xCenter, yCenter, i);
+				}
+			}
+			break;
+			}
+		}
+	}
 }
--- DamageGenerator.h
+++ DamageGenerator.h
@@ -1,58 +1,36 @@
 #pragma once
+#include<random>
+#include"DmgMap.h"
 
-//Qt libraries
 
-//STL libraries
-
-//Own dependencies
-#include "baseFilter.h"
+enum damageKind
+{
+	square,
+	diamond,
+	circle,
+	cloud,
+	snow,
+	fog
+};
 
 class DamageGenerator
 {
-public:
-
-    /// @enum here are storred avaiable damage algorithms, that generates shapes:
-    enum damage_kinds
-    {
-        euclidean,      /// @var squares
-        manhattan,      /// @var ?
-        chaos           /// @var dots
-
-    };
-
-    /// @brief This is static class, no constructors at all
-    DamageGenerator() = delete;
-
-    /// @brief This methode will generate damages, using algorithm setted by DamageGenerator::setKindOfDamage
-    /// @param dst - Here should be orginal picture, it will be destroyed (modifed)
-    /// @param missing_ones - also output parameter. container with modified coordinates
-    /// @param number_of_damages - defines how many times damage should be executed
-    /// @param damage_size - how big single damage should be
-    static void makeDamage(QImage& dst, container& missing_ones, const size_t number_of_damages, const size_t damage_size) noexcept;
-
-    /// @brief This methode render visual representation of given damage
-    /// @param input - container with already damaged coordinates
-    /// @param output - output parameter, white mean missing, black mean normal ones
-    static void renderMapFromRawData(const container& input, QImage& output) noexcept;
-
-    /// @brief Simple setter
-    static void setKindOfDamage(const damage_kinds iKindOfDamage) noexcept { current_damage_kind = iKindOfDamage; }
-
 private:
+	static void rgbChanger(int xAktual, int yAktual, int xChange, int yChange);
+	static void rgbDamage(int xAktual, int yAktual);
+	static void drawCircle(int xCenter, int yCenter);
+	static damageKind kindOfDamage;
+	static int*** tabRGB;
+	static size_t sizeX;
+	static size_t sizeY;
+	static DmgMap* dmgMap;
+	static size_t pixelDamageNumber;
 
-    /// @brief Definition of euclidean damage algorithm, generates square holes
-    /// @param dmg_size - side of square
-    /// @param gen - methode to generate coordinates
-    /// @param make_damage - methode to save damaged pixel
-    /// @warning DEVELOP: If you want to implement another algorithm, signature as below is required to works. don't rewrite param documentation (or just type: same as euclidean)
-    static void euclidean_generator(const int dmg_size, const std::function<coord()>& gen, const std::function<bool(const coord &)> &make_damage) noexcept;
+public:
+    DamageGenerator();
+    ~DamageGenerator();
 
-    /// @brief generates single pixel dots
-    static void chaos_generator(const int dmg_size, const std::function<coord()>& gen, const std::function<bool(const coord &)> &make_damage) noexcept;
-
-    /// @variable what can i say? Enum holder
-    inline static damage_kinds current_damage_kind{ damage_kinds::euclidean };
-
+    static void makeDamage(int***iTabRGB, size_t iSizeX, size_t iSizeY, size_t iDamageCenterNumber, size_t iPixelDamageNumber, DmgMap  &iDmgMap);
+	static void setKindOfDamage(damageKind iKindOfDamage) { kindOfDamage = iKindOfDamage; }
 };
-
 
